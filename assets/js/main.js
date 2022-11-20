9245e16943ebacf421c791ed200faae683210704
Anurag-1427 <kaushikanurag2001@gmail.com>
2022-11-20 19:58:34 +0530

    scroll reveal animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,3 +196,30 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true /* Animations repeat */,
+});
+
+sr.reveal(
+  `.home__data, .projects__container, .testimonial__container, .footer__container`
+);
+sr.reveal(`.home__info div`, { delay: 600, origin: "bottom", interval: 100 });
+sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {
+  // delay: 600,
+  origin: "left",
+  // interval: 100,
+});
+sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {
+  // delay: 600,
+  origin: "right",
+  // interval: 100,
+});
+sr.reveal(`.qualification__content, .services__card`, {
+  // delay: 600,
+  // origin: "right",
+  interval: 100,
+});
