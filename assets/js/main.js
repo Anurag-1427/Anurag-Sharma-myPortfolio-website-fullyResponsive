4fc1f0a3f0b1134a0c765846beac1cffcd662817
Anurag-1427 <kaushikanurag2001@gmail.com>
2022-11-20 19:40:55 +0530

    Now, breakpoints are also added in the dom

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,5 +185,14 @@ themeButton.addEventListener("click", () => {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, and the scroll-header class to the header tag
+
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
